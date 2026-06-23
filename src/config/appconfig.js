2f854e47090b52d7eb7b8213aeb10efb1fea1065
Joey Hofman <joeyhofman1@gmail.com>
2026-06-23 17:16:59 +0200

    fix: api url to production

--- src/config/appconfig.js
+++ src/config/appconfig.js
@@ -1,3 +1,3 @@
 export const config = {
-  apiUrl: import.meta.env.VITE_BACKEND_API_URL ?? 'https://localhost:7215',
+  apiUrl: import.meta.env.VITE_BACKEND_API_URL ?? 'https://api.diagramming.com/',
 }
