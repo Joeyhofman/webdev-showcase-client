3f335494ee9a727755fdf7910fcac4e05ef56456
Joey Hofman <joeyhofman1@gmail.com>
2026-06-26 15:50:08 +0200

    fix: user ? operator for window object

--- src/config/appconfig.js
+++ src/config/appconfig.js
@@ -1,6 +1,6 @@
 export const config = {
   apiUrl:
-    window.APP_CONFIG.apiUrl ??
+    window?.APP_CONFIG?.apiUrl ??
     import.meta.env.VITE_BACKEND_API_URL ??
     'https://api.diagramming.com/',
 }
